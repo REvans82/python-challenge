ac42b1ac5a59ea198a0ac8a370459eaab5bae23d
Robin M. Evans <robinmevans@Robins-MacBook-Pro.local>
2021-02-14 20:42:47 -0500

    creating date set object and profit/loss integer

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,4 +5,22 @@ Created on Wed Feb 10 19:09:08 2021
 
 @author: robinmevans
 """
+date_set=set()
+p_l_int=int()
+fin_file = open("Resources/budget_data.csv")
+for line  in fin_file.readlines()[1:]:
+    print(line.split(','))
+    date_set.add(line.split(',')[0])
+    
+    #x=int(line.split(',')[1][:-2])
+    x=line.split(",")
+    x=x[1]
+    x=x[:-2]
+    x=int(x)
+    p_l_int+=x
 
+    
+    
+    
+print(len(date_set))
+print(p_l_int)
